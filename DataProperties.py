1090aa377b702b742f4da519a6131589363b1318
Pavel Ianko <yanpavevg@yandex.ru>
2022-04-24 09:32:06 +0200

    added modular files

---
+++ DataProperties.py
@@ -0,0 +1,7 @@
+class DataProperties:
+    n_samples = 50 # 200
+    labeled_percentage = 0.05
+    n_dims = 2
+    n_clusters = 2
+
+    unlabeled = -1000000  # To mark the unlabelled data instance
---
+++ GradientSolver.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+class GradientSolver:
+    def __init__(
+        self, 
+        lr_strategy, 
+        learning_rate, 
+        similarity_func, 
+        bcgd_strategy = None
+    ):
+
+        available_lr_strategies = [
+            'lr_constant'
+        ]
+        assert lr_strategy in available_lr_strategies, print(f'Pick available lr_strategy: {available_lr_strategies}')
+
+        self.lr_strategy = lr_strategy
+        self.learning_rate = learning_rate
+        self.similarity_func = similarity_func
+
+        self.losses = []
+        self.n_iterations = 0
+
+
+    def get_learning_rate(self):
+        if self.lr_strategy == 'lr_constant':
+            return self.learning_rate   
+
+    def compute_grad_component(self, X, Y, labeled_idxs, unlabeled_idxs, idx):
+
+        grad_component = 0.0
+            
+        for labeled_idx in labeled_idxs:
+            w_ij = self.calc_weight(X[idx], X[labeled_idx])
+            # w_ij = np.linalg.norm(X[idx] - X[labeled_idx])
+            grad_component += 2 * w_ij * (Y[idx] - Y[labeled_idx])
+
+        for another_unlab_idx in unlabeled_idxs:
+            # w_ij_ = np.linalg.norm(X[idx] - X[another_unlab_idx])
+            w_ij_ = self.calc_weight(X[idx], X[another_unlab_idx])
+            grad_component += 2 * w_ij_ * (Y[idx] - Y[another_unlab_idx])
+
+        return grad_component
+
+
+    def calc_weight(self, Xi, Xj):
+        eps = 0.001
+        return 1 / (np.linalg.norm(Xi - Xj) + eps)  # / np.max([np.linalg.norm(Xi), np.linalg.norm(Xj)])
+
+            
+    def compute_grad(self, X, Y, labeled_idxs, unlabeled_idxs):
+
+        grad = []
+        for unlabeled_idx in unlabeled_idxs:
+            # compute component of gradient for this 
+            # current unlabeled
+            grad_component = self.compute_grad_component(X, Y, labeled_idxs, unlabeled_idxs, idx=unlabeled_idx)
+            grad.append(grad_component)
+
+        return np.array(grad)
+
+    def compute_loss(self, X, Y, labeled_idxs, unlabeled_idxs):
+        res = 0
+        
+        for labeled_idx in labeled_idxs:
+            for unlab_idx in unlabeled_idxs:
+                w_ij = self.calc_weight(X[labeled_idx], X[unlab_idx])
+                # w_ij = np.linalg.norm(X[labeled_idx] - X[unlab_idx])
+                res += w_ij * ((Y[labeled_idx] - Y[unlab_idx]) ** 2)
+        
+        for unlab_idx in unlabeled_idxs:
+            for another_unlab_idx in unlabeled_idxs:
+                w_ij = self.calc_weight(X[unlab_idx], X[another_unlab_idx])
+                # w_ij = np.linalg.norm(X[unlab_idx] - X[another_unlab_idx])
+                res += 0.5 * w_ij * ((Y[unlab_idx] - Y[another_unlab_idx]) ** 2)
+        
+        return res
+    
+    def threshold_proc(self, Y_preds):
+        Y_preds[Y_preds > 0.5] = 1.0
+        Y_preds[Y_preds <= 0.5] = 0.0
+        return Y_preds
