2beb69790a50174a203f0be992a0b71868b9b1ee
Pavel Ianko <yanpavevg@yandex.ru>
2022-04-24 09:19:15 +0200

    Utils.py

---
+++ Utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_clusters(X, Y):
+    fig, ax = plt.subplots()
+
+    clusters = np.unique(Y)
+    for c in clusters:
+        mask = Y == c
+        subset = X[mask]
+        ax.scatter(
+            subset[:, 0],
+            subset[:, 1], 
+            marker = 'o'
+        )
+
+def delabel_data (labeled_data, labeled_percent, delabel_value):
+    res = np.ndarray.copy(labeled_data)
+    clusters_ids = np.unique(labeled_data)
+
+    for cluster_id in clusters_ids:
+        # find indices of object, labeled as cluster_id
+        # we keep 'labeled_percent' points still labeled
+
+        cluster_indices = np.where(labeled_data == cluster_id)[0]
+        print(f'Found {len(cluster_indices)} instances of object {cluster_id}')
+
+        n_to_delabel = int(len(cluster_indices) * (1 - labeled_percent))
+        print(f'We have to delabel {n_to_delabel} instances')
+
+        # we choose data to delabel randomly
+        indices_to_delabel = np.random.choice(
+            len(cluster_indices - 1),
+            n_to_delabel,
+            replace = False
+        )
+
+        assert len(indices_to_delabel) == n_to_delabel, print(len(indices_to_delabel))
+        assert np.max(indices_to_delabel) < len(res)
+        assert np.max(indices_to_delabel) <= np.max(cluster_indices)
+
+        res[
+            cluster_indices[indices_to_delabel]
+        ] = delabel_value
+    return res
