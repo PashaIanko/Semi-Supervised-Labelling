7909744c21e1d1d59928ecff24123a294283f549
Pavel Ianko <yanpavevg@yandex.ru>
2022-04-24 12:31:21 +0200

    optimized GD method

--- GradientDescentSolver.py
+++ GradientDescentSolver.py
@@ -20,23 +20,19 @@ class GradientDescentSolver(GradientSolver):
         loss_prev = 0.0
         self.losses = []
         self.n_iterations = 0
+
+        assert(self.lr_strategy == 'lr_constant')  # optimization, now work with lr==const
+        learning_rate = self.get_learning_rate()
+        
         for i in range(iter_limit):
             loss = self.compute_loss(X, Y_res, labeled_idxs, unlabeled_idxs)
             self.losses.append(loss)
             delta_loss = abs(loss - loss_prev)
             print(f'Loss: {loss}, delta loss: {delta_loss}')
 
-
-            if ((i > 0 and delta_loss < delta_loss_limit) or (loss < stop_loss)):
-                break
-            else:
-                grad = self.compute_grad(X, Y_res, labeled_idxs, unlabeled_idxs)
-                learning_rate = self.get_learning_rate()
-                updates = -learning_rate * grad
-
-                
-                for i in range(len(grad)):
-                    Y_res[unlabeled_idxs[i]] += updates[i]
+            if not ((delta_loss < delta_loss_limit) or (loss < stop_loss)):
+                updates = -learning_rate * self.compute_grad(X, Y_res, labeled_idxs, unlabeled_idxs)
+                for i in range(len(updates)): Y_res[unlabeled_idxs[i]] += updates[i] 
                 
                 loss_prev = loss
                 self.n_iterations += 1
